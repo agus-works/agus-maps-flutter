9d37dabfc4952cc52a0dc81989ab4d03cb082381
gil.regalado <gil.regalado@netzontech.com>
2025-12-17 13:56:37 +0800

    chore: Initialize repository.

---
+++ example/android/app/src/main/kotlin/app/agus/maps/agus_maps_flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.agus.maps.agus_maps_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/agus_maps_flutter.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../agus_maps_flutter.podspec for more information.
+#include "../../src/agus_maps_flutter.c"
