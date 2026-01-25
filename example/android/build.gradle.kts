673e2a125abe5c919e83d8648dcd2e290d7062aa
bangonkali <bangonkali@gmail.com>
2026-01-25 09:45:00 +0800

    build: Add Gradle wrapper files for version 8.14
    - Created gradle-wrapper.properties to specify the Gradle distribution URL and configuration.
    - Added gradlew shell script for Unix-based systems to facilitate Gradle execution.
    - Introduced gradlew.bat for Windows to enable Gradle commands in a Windows environment.

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -14,7 +14,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        project.extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+             compileSdkVersion(36)
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
